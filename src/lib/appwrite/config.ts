38cf11a93eb25d3ce7e3ef6cc6e32c307a570cba
Karim Patatas <66632918+Sen2pi@users.noreply.github.com>
2023-12-18 00:50:24 +0000

    base de dados na Appwrite , implementação da auth com a base de dados

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -3,6 +3,18 @@ import {Client, Account , Databases, Storage, Avatars} from 'appwrite'
 export const appwriteConfig ={
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
     url: import.meta.env.VITE_APPWRITE_URL,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    ficheiroCollectionId: import.meta.env.VITE_APPWRITE_FICHEIRO_COLLECTIONS_ID,
+    historicoCollectionId: import.meta.env.VITE_APPWRITE_HISTORICO_COLLECTIONS_ID,
+    mensagemCollectionId: import.meta.env.VITE_APPWRITE_MENSAGEM_COLLECTIONS_ID,
+    chatCollectionId: import.meta.env.VITE_APPWRITE_CHAT_COLLECTIONS_ID,
+    requesitoCollectionId: import.meta.env.VITE_APPWRITE_REQUESITO_COLLECTIONS_ID,
+    grupoCollectionId: import.meta.env.VITE_APPWRITE_GRUPO_COLLECTIONS_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTIONS_ID,
+    inscricaoCollectionId: import.meta.env.VITE_APPWRITE_INSCRICAO_COLLECTIONS_ID,
+    tarefaCollectionId: import.meta.env.VITE_APPWRITE_TAREFA_COLLECTIONS_ID,
+    disciplinaCollectionId: import.meta.env.VITE_APPWRITE_DISCIPLINA_COLLECTIONS_ID,
 }
 
 export const client = new Client();
